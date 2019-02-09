d68b3fabdac35db198067f7d56db2cca5c30ad59
pleelapr <p.lee@outlook.com>
2019-02-09 14:55:31 -0500

    edit sentimentAnalysis.py

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -1,21 +1,22 @@
 import pandas as pd
 import csv
 import os
+import re
 
 PATH_RESULT = 'result'
 
 def clean_tweet(tweet): 
-        ''' 
-        Utility function to clean tweet text by removing links, special characters 
-        using simple regex statements. 
-        '''
-        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+	return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
 tweets = pd.DataFrame()
 for filename in os.listdir(PATH_RESULT):
 	data = pd.read_csv(PATH_RESULT+'/'+filename)
-	print(data)
-	tweets.append(data)
+	# print(data)
+	tweets = tweets.append(data)
 
 print(tweets[0:6])
 
+for i in range(len(tweets)):
+	tweets.loc[i,'tweetText'] = clean_tweet(tweets.loc[i,'tweetText'])
+
+print(tweets[0:6])
